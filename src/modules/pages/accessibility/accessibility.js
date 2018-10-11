c53f977761d0476361c6820af0336d87f2d32837
Robert Beatty <beattyre1@gmail.com>
2018-10-11 15:54:13 -0400

    Section descriptions added

--- src/modules/pages/accessibility/accessibility.js
+++ src/modules/pages/accessibility/accessibility.js
@@ -3,21 +3,18 @@ import { styles } from '../map/map_styles/accessibility.js'
 
 const LoadLayers = (map, styles) =>{
   for (let source in styles){
-    map.addSource(source, {
-      type: styles[source].type,
-      url: styles[source].url
-    })
+    map.addSource(source, styles[source].sourceDef)
     for (let layer in styles[source].layers){
       let thisLayer = styles[source].layers[layer]
       let layerDef = {
         "id": `${source}-${layer}`,
         "type": thisLayer.type,
         "source": source,
-        "source-layer": layer,
         "paint": thisLayer.paint
       }
       !thisLayer.filter ? null : layerDef['filter'] = thisLayer.filter
       !thisLayer.layout ? null : layerDef['layout'] = thisLayer.layout
+      styles[source].sourceDef.type != 'vector' ? null : layerDef['source-layer'] = thisLayer.source
       map.addLayer(layerDef)
     }
   }
@@ -50,6 +47,7 @@ const BuildPage = props =>{
     let textBox = document.createElement('div')
     textBox.classList.add('accessibility-text')
     textBox.id = `accessibilityText-${section}`
+    textBox.innerHTML = props.sections[section].title+props.sections[section].description
     let mapBox = document.createElement('div')
     mapBox.classList.add('accessibility-map')
     mapBox.id = `accessibilityMap-${section}`
@@ -68,23 +66,25 @@ class Accessibility{
       container: document.querySelector('#main'),
       sections: {
         allAccessibility: {
-          title: 'Accessibility from All Rail Stations'
+          title: '<h3>Accessibility from All Rail Stations</h3>',
+          description: '<p>This shows the number of destinations reachable via rail from each zone for passengers that can use all rail stations. The results are mapped by TAZ. The darker the blue, the more destinations are reachable from that TAZ.</p>'
         },
         currentAccessibility:{
-          title: 'Accessible Destinations using Only Wheelchair Accessible Stations'
+          title: '<h3>Accessible Destinations using Only Wheelchair Accessible Stations</h3>',
+          description: '<p>The next map shows the destinations reachable when you can only use wheelchair accessible stations. So same thing here – the darker the blue, the more destinations are reachable via rail. However, you can see that not only is there a lot less dark blue, there is a lot less blue overall- meaning that there when you can’t use inaccessible stations, there are a lot of destinations you cannot reach via rail.</p>'
         },
         currentDisparity:{
-          title: 'Destination Disparity for Wheelchair users in Comparison with All Users'
+          title: '<h3>Destination Disparity for Wheelchair users in Comparison with All Users</h3>',
+          description: '<p>This map highlights the differences between the baseline and the wheelchair accessible map. The darker the orange, the larger the gap in reachable destinations for wheelchair users. So the darkest places are those where wheelchair users lose out on the most destinations due to inaccessible stations and where improvements should be focused.</p>'
         },
         futureAccessibility:{
-          title: 'Destinations Accessible using Accessible Stations or Those with Programmed Improvements'
+          title: '<h3>Destinations Accessible using Accessible Stations or Those with Programmed Improvements</h3>',
+          description: '<p>We know that there are a number of stations that are programmed for wheelchair accessibility improvements or have improvements in progress. So this map considers those. This one includes stations that are currently accessible and those that are programmed for improvement. Again, the darker the blue, the more destinations are reachable via rail from that zone.</p>'
         },
         futureDisparity:{
-          title: 'Destination Disparity using Accessible Stations or Those with Programmed Improvements'
-        },
-        relativeDisparity:{
-          title: 'Relative Benefit of Programmed Accessibility Improvements'
-        },
+          title: '<h3>Destination Disparity using Accessible Stations or Those with Programmed Improvements</h3>',
+          description: '<p>And this maps shows where the gaps remain – in the darkest orange. So these are the places that we would want to focus on for the next batch of wheelchair accessibility improvements.</p>'
+        }
       }
     }
     this.render()
--- src/modules/pages/map/map_styles/accessibility.js
+++ src/modules/pages/map/map_styles/accessibility.js
@@ -1,16 +1,12 @@
 const styles = {
   base: {
-    type: 'vector',
-    url: 'https://tiles.dvrpc.org/data/dvrpc-freight-story.json',
+    sourceDef: {
+      type: 'vector',
+      url: 'https://tiles.dvrpc.org/data/dvrpc-freight-story.json'
+    },
     layers: {
-      interstates: {
-        type: 'line',
-        paint: {
-          'line-width': 1.75,
-          'line-color': '#cccccc'
-        },
-      },
-      county: {
+      countyFill: {
+        source: 'county',
         type: 'fill',
         filter: [
           '==',
@@ -18,11 +14,33 @@ const styles = {
           'Yes'
         ],
         paint: {
-          "fill-color": 'rgba(216,199,46,.1)',
-          'fill-outline-color': '#08506d'
+          "fill-color": '#fcfcfc',
+          'fill-opacity': .5
         }
       },
-      "dvrpc-places" : {
+      countyOutline: {
+        source: 'county',
+        type: 'line',
+        filter: [
+          '==',
+          'DVRPC_REG',
+          'Yes'
+        ],
+        paint: {
+          "line-color" : "#e89234",
+          "line-width": .5
+        }
+      },
+      interstates: {
+        type: 'line',
+        paint: {
+          'line-width': .5,
+          'line-color': '#cccccc'
+        },
+        source: 'interstates'
+      },
+      labels : {
+        source: 'dvrpc-places',
         type: 'symbol',
         layout: {
           "text-field": "{NAME}",
@@ -38,7 +56,24 @@ const styles = {
               "text-halo-width": 2,
               "text-halo-blur": 3
         }
+      },
+    }
+  },
+  transitLines: {
+    sourceDef: {
+      type: 'geojson',
+      data: 'https://opendata.arcgis.com/datasets/5af7a3e9c0f34a7f93ac8935cb6cae3b_0.geojson'
+    },
+    layers: {
+      railLines: {
+        type: 'line',
+        paint: {
+          "line-color": '#000',
+          "line-width": 1.5
+        }
+
       }
+
     }
   }
 }
