aff77fe2fc2762ca74f331abcb527f3d41906bb8
Robert Beatty <beattyre1@gmail.com>
2019-02-13 10:46:22 -0500

    Move api folder to root

---
+++ api/rtps/urls.py
@@ -0,0 +1,25 @@
+"""rtps URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls,),
+    url(r'^api/rtps/gap\?*', include('gap.urls')),
+    url(r'^api/rtps/access\?*', include('access.urls')),
+    url(r'^api/rtps/frequency\?*', include('frequency.urls')),
+    url(r'^api/rtps/reliability\?*', include('reliability.urls'))
+]
