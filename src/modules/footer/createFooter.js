9948118245ff5861a50b25653d97e98328deed14
Marc Molta <mcmolta10@gmail.com>
2019-06-27 14:01:51 -0400

    add link to dvrpc homepage on dvrpc logos close #46

--- src/modules/footer/createFooter.js
+++ src/modules/footer/createFooter.js
@@ -6,24 +6,30 @@ const createFooter = () => {
     // fragment for logo + DVRPC address info
     const addressFragment = document.createDocumentFragment()
     const addressContactWrapper = document.createElement('div')
+    const footerLogoLink = document.createElement('a')
     const footerLogo = document.createElement('img')
     const address = document.createElement('address')
     const dvrpcContact = document.createElement('p')
 
     addressContactWrapper.classList.add('footer-content-wrapper')
+    footerLogoLink.id = 'footer-logo-link'
     footerLogo.id = 'footer-logo'
     address.id = 'footer-address'
 
+    footerLogoLink.href="https://www.dvrpc.org/"
+    footerLogoLink.target = "blank"
     footerLogo.src = logo
     footerLogo.alt = 'dvrpc logo'
 
     address.textContent = '190 N. Independence Mall W, \r\n 8th Floor \r\n Philadelphia, PA 19106'
     dvrpcContact.innerHTML = '<strong>Phone</strong>: (215) - 592 - 1800'
 
+    footerLogoLink.appendChild(footerLogo)
+
     addressContactWrapper.appendChild(address)
     addressContactWrapper.appendChild(dvrpcContact)
     
-    addressFragment.appendChild(footerLogo)
+    addressFragment.appendChild(footerLogoLink)
     addressFragment.appendChild(addressContactWrapper)
 
     // fragment for Sarah's contact info
